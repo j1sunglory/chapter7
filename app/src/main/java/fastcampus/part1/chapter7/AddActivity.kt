4b0c3a82d7673a6f3180c6c6b79c8be26a88326e
Jisung Yoon <jsyooon0126@gmail.com>
2023-10-28 17:37:46 +0900

    Initial commit

---
+++ app/src/main/java/fastcampus/part1/chapter7/AddActivity.kt
@@ -0,0 +1,86 @@
+package fastcampus.part1.chapter7
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.children
+import com.google.android.material.chip.Chip
+import fastcampus.part1.chapter7.databinding.ActivityAddBinding
+
+class AddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBinding
+    private var originWord : Word? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+        binding.addButton.setOnClickListener {
+            if(originWord == null) add() else edit()
+        }
+
+    }
+
+    //chipGroup에 대한 리스트 뷰 (품사)
+    private fun initViews() {
+        val types = listOf("명사", "대명사", "동사", "전치사", "접속사", "형용사", "전치사", "부사")
+        binding.typeChipGroup.apply {
+            types.forEach { text -> addView(createChip(text)) }
+        }
+
+        originWord = intent.getParcelableExtra<Word>("originWord")
+        originWord?.let { word ->
+            binding.textInputEditText.setText(word.text)
+            binding.meanTextInputEditText.setText(word.mean)
+            val selectedChip =
+                binding.typeChipGroup.children.firstOrNull { (it as Chip).text == word.type } as? Chip
+            selectedChip?.isChecked = true
+        }
+    }
+
+    private fun createChip(text: String): Chip {
+        return Chip(this).apply {
+            setText(text)
+            isCheckable = true
+            isClickable = true
+        }
+    }
+
+    private fun add() {
+        val text = binding.textInputEditText.text.toString()
+        val mean = binding.meanTextInputEditText.text.toString()
+        val type = findViewById<Chip>(binding.typeChipGroup.checkedChipId).text.toString()
+        val word = Word(text, mean, type)
+
+        Thread {
+            AppDatabase.getInstance(this)?.wordDao()?.insert(word)
+            runOnUiThread {
+                Toast.makeText(this, "저장을 완료했습니다.",Toast.LENGTH_SHORT).show()
+            }
+            val intent = Intent().putExtra("isUpdated", true)
+            setResult(RESULT_OK, intent)
+            finish()
+        }.start()
+
+    }
+
+    private fun edit() {
+        val text = binding.textInputEditText.text.toString()
+        val mean = binding.meanTextInputEditText.text.toString()
+        val type = findViewById<Chip>(binding.typeChipGroup.checkedChipId).text.toString()
+        val editWord = originWord?.copy(text = text, mean=mean, type = type)
+
+        Thread {
+            editWord?.let { word ->
+                AppDatabase.getInstance(this)?.wordDao()?.update(word)
+                val intent = Intent().putExtra("editWord", editWord)
+                setResult(RESULT_OK, intent)
+                runOnUiThread { Toast.makeText(this, "수정을 완료했습니다.", Toast.LENGTH_SHORT).show() }
+                finish()
+            }
+        }.start()
+    }
+}
